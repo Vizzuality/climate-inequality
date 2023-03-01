1a91365dbe27e3c3dfa2eac82e046ba2000308ed
Alvaro Leal <pursuance@gmail.com>
2023-03-01 15:13:46 +0100

    Add fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 const forms = require('@tailwindcss/forms');
 const lineClamp = require('@tailwindcss/line-clamp');
 
@@ -11,5 +13,54 @@ module.exports = {
     './src/containers/**/*.@(tsx|ts)',
     './src/pages/**/*.tsx',
   ],
+  theme: {
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '1rem',
+        sm: '2rem',
+        lg: '4rem',
+        xl: '5rem',
+        '2xl': '6rem',
+      },
+    },
+    colors: {
+      black: '#000',
+      white: '#fff',
+      100: '#fff9d4',
+      200: '#fff3a9',
+      300: '#ffed7e',
+      400: '#ffe753',
+      500: '#ffe229',
+      600: '#ccb420',
+      700: '#998718',
+      800: '#665a10',
+      900: '#332d08',
+    },
+    fontSize: {
+      '2xs': '0.625rem',
+      xs: '0.75rem',
+      sm: '0.875rem',
+      base: '1rem',
+      lg: '1.25rem',
+      xl: '1.5rem',
+      '2xl': '2rem',
+    },
+    fontFamily: {
+      sans: 'Inter, sans-serif',
+      serif: 'Baskerville, serif',
+    },
+    borderRadius: {
+      none: '0',
+      xs: '0.0625rem',
+      sm: '0.125rem',
+      default: '0.3125rem',
+      lg: '0.5rem',
+      xl: '1.25rem',
+      '2xl': '1.5rem',
+      '3xl': '2rem',
+      full: '9999px',
+    },
+  },
   plugins: [forms, lineClamp],
 };
