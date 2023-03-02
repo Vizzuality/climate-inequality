15f715d3192ecabcdfdf69a86222c284703097a0
Alvaro Leal <pursuance@gmail.com>
2023-03-02 14:05:32 +0100

    FIx icon types

--- src/components/icon/component.tsx
+++ src/components/icon/component.tsx
@@ -4,17 +4,21 @@ import cx from 'classnames';
 
 import type { IconProps } from './types';
 
-export const Icon: FC<IconProps> = ({ icon, className = 'w-5 h-5', style }: IconProps) => (
-  <svg
-    className={cx({
-      'fill-current': true,
-      [className]: className,
-    })}
-    viewBox={icon?.viewBox || '0 0 32 32'}
-    style={style}
-  >
-    <use xlinkHref={`#${icon?.id || icon}`} />
-  </svg>
-);
+export const Icon: FC<IconProps> = ({ icon, className = 'w-5 h-5', style }: IconProps) => {
+  const viewBox = typeof icon === 'string' ? '0 0 32 32' : icon.viewBox;
+  const id = typeof icon === 'string' ? icon : icon.id;
+  return (
+    <svg
+      className={cx({
+        'fill-current': true,
+        [className]: className,
+      })}
+      viewBox={viewBox}
+      style={style}
+    >
+      <use xlinkHref={`#${id}`} />
+    </svg>
+  );
+};
 
 export default Icon;
--- src/components/icon/types.d.ts
+++ src/components/icon/types.d.ts
@@ -1,8 +1,10 @@
+type SingleIcon = {
+  id: string;
+  viewBox: string;
+};
+
 export interface IconProps {
-  icon: {
-    id: string;
-    viewBox: string;
-  };
+  icon: SingleIcon | string;
   className?: string;
   style?: unknown;
 }
