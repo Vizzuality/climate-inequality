c990a0ffa74211c4175b89f22f55128d58224968
Alvaro Leal <pursuance@gmail.com>
2023-06-02 15:59:39 +0200

    Fix meta tags

--- src/components/meta-tags/meta-tags-component.tsx
+++ src/components/meta-tags/meta-tags-component.tsx
@@ -1,7 +1,6 @@
 import { FC } from 'react';
 
 import Head from 'next/head';
-import { useRouter } from 'next/router';
 
 export interface MetaTagsProps {
   title: string;
@@ -28,8 +27,6 @@ const MetaTags: FC<MetaTagsProps> = ({
   type = 'website',
   imageURL,
 }: MetaTagsProps) => {
-  const { asPath } = useRouter();
-
   const BASE_URL = getBaseUrl();
 
   return (
@@ -40,34 +37,39 @@ const MetaTags: FC<MetaTagsProps> = ({
       />
       <meta name="author" content="Vizzuality" />
       <meta name="robots" content="index, follow" />
-      <meta name="viewport" content="width=device-width" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
       <link rel="icon" href="/favicon.ico" type="image/x-icon" />
-      <link rel="apple-touch-icon" type="image/png" sizes="72x72" href="/apple-touch-icon.png" />
-      <link rel="icon" type="image/png" sizes="192x192" href="/android-chrome-192x192.png" />
-      <link rel="icon" type="image/png" sizes="256x256" href="/android-chrome-256x256.png" />
-      <link rel="manifest" href={`${process.env.NEXT_PUBLIC_BASE_PATH}/manifest.json`} />
+      <link
+        rel="apple-touch-icon"
+        type="image/png"
+        sizes="72x72"
+        href="/images/apple-touch-icon.png"
+      />
+      <link rel="icon" type="image/png" sizes="192x192" href="/images/android-chrome-192x192.png" />
+      <link rel="icon" type="image/png" sizes="256x256" href="/images/android-chrome-256x256.png" />
+      <link rel="manifest" href={`/${BASE_URL}/manifest.json`} />
       <meta name="msapplication-TileColor" content="#ffffff" />
       <meta
         name="msapplication-TileImage"
-        content={`${process.env.NEXT_PUBLIC_BASE_PATH}/ms-tile-150x150.png`}
+        content={`${process.env.NEXT_PUBLIC_BASE_PATH}/images/ms-tile-150x150.png`}
       />
       <meta name="theme-color" content="#ffffff" />
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:title" content={title} />
       <meta name="twitter:description" content={description} />
       <meta name="twitter:image" content={`${BASE_URL}/${imageURL}`} />
-      <meta name="twitter:domain" content="https://www.climate-inequality.vizzuality.com" />
+      <meta name="twitter:domain" content={`${BASE_URL}`} />
       <meta name="twitter:site" content="@vizzuality" />
       <meta name="twitter:creator" content="@vizzuality" />
       <meta name="og:title" content={title} />
       <meta name="og:description" content={description} />
       <meta name="og:type" content={type} />
-      <meta name="og:url" content={`${BASE_URL}${asPath}`} />
+      <meta name="og:url" content={`${BASE_URL}`} />
       <meta property="og:locale" content="en_US" />
       <meta name="og:image" content={`${BASE_URL}/${imageURL}`} />
       <meta property="og:site_name" content="Climate Inequality" />
-      <meta name="msapplication-config" content="/browserconfig.xml" />
-      <link rel="manifest" href="/site.webmanifest" />
+      <meta name="msapplication-config" content="/images/browserconfig.xml" />
+      <link rel="manifest" href="/images/site.webmanifest" />
     </Head>
   );
 };
