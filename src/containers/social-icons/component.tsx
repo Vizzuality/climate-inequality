9f20579bf0662abf268b290ebc2b79077f9fb8dc
Alvaro Leal <pursuance@gmail.com>
2023-06-22 17:31:08 +0200

    Add copy button

--- src/containers/social-icons/component.tsx
+++ src/containers/social-icons/component.tsx
@@ -1,11 +1,23 @@
+import { useState } from 'react';
+
 import Icon from 'components/icon';
 
 import Facebook from 'svgs/social/facebook.svg';
 import Github from 'svgs/social/github.svg';
 import Linkedin from 'svgs/social/linkedin.svg';
 import Twitter from 'svgs/social/twitter.svg';
+import Share from 'svgs/ui/copy.svg';
+import Correct from 'svgs/ui/correct.svg';
 
-const socialData = [
+interface SocialData {
+  href?: string;
+  icon: 'string | SingleIcon';
+  afterIcon?: 'string | SingleIcon';
+  shareLink?: string;
+  action?: 'copy';
+}
+
+const socialData: SocialData[] = [
   {
     href: 'https://twitter.com/Vizzuality',
     icon: Twitter,
@@ -28,6 +40,11 @@ const socialData = [
     icon: Github,
     shareLink: 'https://github.com/Vizzuality',
   },
+  {
+    icon: Share,
+    action: 'copy',
+    afterIcon: Correct,
+  },
 ];
 
 type SocialIconsProps = {
@@ -36,16 +53,35 @@ type SocialIconsProps = {
 };
 
 const SocialIcons = ({ className, isShare }: SocialIconsProps) => {
+  const [copied, setCopied] = useState(false);
+  const handleCopy = async (): Promise<void> => {
+    try {
+      await navigator.clipboard.writeText('https://climate-inequality.vizzuality.com/quiz');
+      setCopied(true);
+    } catch (err) {
+      console.error('Failed to copy: ', err);
+    }
+  };
+
   return (
     <div className={className}>
-      {socialData.map(({ href, icon: IconSvg, shareLink }) => (
-        <a key={href} href={isShare ? shareLink : href} target="_blank" rel="noreferrer">
-          <Icon
-            icon={IconSvg}
-            className="h-6 w-6 transition-all duration-500 ease-in-out hover:fill-400"
-          />
-        </a>
-      ))}
+      {socialData.map(({ href, icon: IconSvg, shareLink, action, afterIcon: AfterIconSvg }) =>
+        action ? (
+          <button key={href} onClick={handleCopy}>
+            <Icon
+              icon={copied ? AfterIconSvg : IconSvg}
+              className="h-6 w-6 transition-all duration-500 ease-in-out hover:text-400"
+            />
+          </button>
+        ) : (
+          <a key={href} href={isShare ? shareLink : href} target="_blank" rel="noreferrer">
+            <Icon
+              icon={IconSvg}
+              className="h-6 w-6 transition-all duration-500 ease-in-out hover:fill-400"
+            />
+          </a>
+        )
+      )}
     </div>
   );
 };
